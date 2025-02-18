68de1c60663b66d1b2a6dc054314dd251e4a2b3d
binh <binhtdhe150589@fpt.edu.vn>
2025-02-18 09:39:14 +0700

    video18

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,12 @@
+import { Outlet } from "react-router-dom"
+import AppHeader from "./components/layout/app.header"
 
 function Layout() {
 
   return (
     <>
-      <div>
-        hello
-      </div>
+      <AppHeader />
+      <Outlet />
     </>
   )
 }
