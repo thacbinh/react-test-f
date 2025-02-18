3ca8cf5b88da4abbcc9b9e7f1d0bfec050e30299
binh <binhtdhe150589@fpt.edu.vn>
2025-02-18 16:34:05 +0700

    video30

--- src/pages/client/auth/register.tsx
+++ src/pages/client/auth/register.tsx
@@ -1,8 +1,92 @@
+import type { FormProps } from 'antd';
+import { Button, Divider, Form, Input } from 'antd';
+import './register.scss';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { loginAPI } from '@/services/api';
+type FieldType = {
+    fullName?: string;
+    password?: string;
+    email?: string;
+    phone?: string;
+};
+
+
+
+
 const Register = () => {
+    const [isSubmit, setIsSubmit] = useState(false);
+    const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+        const res = await loginAPI("user@gmail.com", "123456");
+        console.log(">>> check res: ", res)
+        console.log('Success:', values);
+    };
     return (
-        <>
-            <div>Register</div>
-        </>
+        <div className="register-page">
+            <main className="main">
+                <div className="container">
+                    <section className="wrapper">
+                        <div className="heading">
+                            <h2 className="text text-large">Đăng Ký Tài Khoản</h2>
+                            <Divider />
+                        </div>
+                        <Form
+                            name="form-register"
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Họ tên"
+                                name="fullName"
+                                rules={[{ required: true, message: 'Họ tên không được để trống!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Email"
+                                name="email"
+                                rules={[
+                                    { required: true, message: 'Email không được để trống!' },
+                                    { type: "email", message: "Email không đúng định dạng!" }
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[{ required: true, message: 'Mật khẩu không được để trống!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Số điện thoại"
+                                name="phone"
+                                rules={[{ required: true, message: 'Số điện thoại không được để trống!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" loading={isSubmit}>
+                                    Đăng ký
+                                </Button>
+                            </Form.Item>
+                            <Divider>Or</Divider>
+                            <p className="text text-normal" style={{ textAlign: "center" }}>
+                                Đã có tài khoản ?
+                                <span>
+                                    <Link to='/login' > Đăng Nhập </Link>
+                                </span>
+                            </p>
+                        </Form>
+                    </section>
+                </div>
+            </main>
+        </div>
     )
 }
 export default Register;
