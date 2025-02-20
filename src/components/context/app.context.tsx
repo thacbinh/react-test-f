784c0723341f1ccb888c8ca7a05f56270b8f8c80
binh <binhtdhe150589@fpt.edu.vn>
2025-02-20 16:31:41 +0700

    video37

--- src/components/context/app.context.tsx
+++ src/components/context/app.context.tsx
@@ -4,7 +4,9 @@ type TAppContext = {
     isAuthenticated: boolean;
     setIsAuthenticated: (p: boolean) => void;
     user: IUser | null;
-    setUser: (p: IUser) => void
+    setUser: (p: IUser) => void;
+    isAppLoading: boolean;
+    setIsAppLoading: (p: boolean) => void;
 };
 
 type TProps = {
@@ -17,9 +19,10 @@ export const CurrentAppContext = createContext<TAppContext | null>(null);
 export const AppProvider = (props: TProps) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [user, setUser] = useState<IUser | null>(null);
+    const [isAppLoading, setIsAppLoading] = useState<boolean>(true);
 
     return (
-        <CurrentAppContext.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser }}>
+        <CurrentAppContext.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser, isAppLoading, setIsAppLoading }}>
             {props.children}
         </CurrentAppContext.Provider>
     )
--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,21 @@
 import { Outlet } from "react-router-dom"
 import AppHeader from "./components/layout/app.header"
+import { useCurrentApp } from "./components/context/app.context"
+import { useEffect } from "react";
+import { fetchAccountAPI } from "./services/api";
 
 function Layout() {
-
+  const { setUser, isAppLoading, setIsAppLoading } = useCurrentApp();
+  useEffect(() => {
+    const fetchAccount = async () => {
+      const res = await fetchAccountAPI();
+      if (res.data) {
+        setUser(res.data.user);
+      }
+      setIsAppLoading(false)
+    }
+    fetchAccount();
+  }, [])
   return (
     <>
       <AppHeader />
