9aa743ea96ca3d8252a0f87d32b7369293b51413
binh <binhtdhe150589@fpt.edu.vn>
2025-02-20 16:43:26 +0700

    video38

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,14 +3,16 @@ import AppHeader from "./components/layout/app.header"
 import { useCurrentApp } from "./components/context/app.context"
 import { useEffect } from "react";
 import { fetchAccountAPI } from "./services/api";
+import { PacmanLoader } from "react-spinners";
 
 function Layout() {
-  const { setUser, isAppLoading, setIsAppLoading } = useCurrentApp();
+  const { setUser, setIsAuthenticated, isAppLoading, setIsAppLoading } = useCurrentApp();
   useEffect(() => {
     const fetchAccount = async () => {
       const res = await fetchAccountAPI();
       if (res.data) {
         setUser(res.data.user);
+        setIsAuthenticated(true);
       }
       setIsAppLoading(false)
     }
@@ -18,8 +20,27 @@ function Layout() {
   }, [])
   return (
     <>
-      <AppHeader />
-      <Outlet />
+      {
+        isAppLoading === false ?
+          <div>
+            <AppHeader />
+            <Outlet />
+          </div>
+          :
+          <div style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)"
+          }}>
+            <PacmanLoader
+              size={30}
+              color="#36d6b4"
+            />
+          </div>
+      }
+
+
     </>
   )
 }
