0611a51d22e8678c0b94fe579dd91882ec0d8a96
binh <binhtdhe150589@fpt.edu.vn>
2025-02-25 15:37:42 +0700

    danh sach user

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -1,9 +1,9 @@
 import { getUsersAPI } from '@/services/api';
-import { PlusOutlined } from '@ant-design/icons';
+import { DeleteTwoTone, EditTwoTone, PlusOutlined } from '@ant-design/icons';
 import type { ActionType, ProColumns } from '@ant-design/pro-components';
 import { ProTable, TableDropdown } from '@ant-design/pro-components';
 import { Button, Space, Tag } from 'antd';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 
 const columns: ProColumns<IUserTable>[] = [
@@ -15,6 +15,12 @@ const columns: ProColumns<IUserTable>[] = [
     {
         title: '_id',
         dataIndex: '_id',
+        hideInSearch: true,
+        render(dom, entity, index, action, schema) {
+            return (
+                <a href='#'>{entity._id}</a>
+            )
+        },
     },
     {
         title: 'Full Name',
@@ -23,14 +29,40 @@ const columns: ProColumns<IUserTable>[] = [
     {
         title: 'Email',
         dataIndex: 'email',
+        copyable: true
     },
     {
         title: 'Create At',
         dataIndex: 'createdAt',
+
+    },
+    {
+        title: 'Action',
+        hideInSearch: true,
+        render(dom, entity, index, action, schema) {
+            return (
+                <>
+                    <EditTwoTone
+                        twoToneColor="#f57800"
+                        style={{ cursor: "pointer", marginRight: 15 }}
+                    />
+                    <DeleteTwoTone
+                        twoToneColor="#ff4d4f"
+                        style={{ cursor: "pointer" }}
+                    />
+                </>
+            )
+        },
     },
 ];
 
 const TableUser = () => {
+    const [meta, setMeta] = useState({
+        current: 1,
+        pageSize: 5,
+        pages: 0,
+        total: 0
+    })
     const actionRef = useRef<ActionType>();
     return (
         <>
@@ -41,12 +73,14 @@ const TableUser = () => {
                 request={async (params, sort, filter) => {
                     console.log(sort, filter);
                     const res = await getUsersAPI();
+                    if (res.data) {
+                        setMeta(res.data.meta);
+                    }
                     return {
-                        // data: data.data,
                         data: res.data?.result,
-                        "page": 1,
-                        "success": true,
-                        "total": res.data?.meta.total
+                        page: 1,
+                        success: true,
+                        total: res.data?.meta.total
                     }
 
                 }}
@@ -60,10 +94,14 @@ const TableUser = () => {
                         console.log('value: ', value);
                     },
                 }}
-                rowKey="id"
+                rowKey="_id"
                 pagination={{
-                    pageSize: 5,
-                    onChange: (page) => console.log(page),
+                    current: meta.current,
+                    pageSize: meta.pageSize,
+                    showSizeChanger: true,
+                    total: meta.total,
+                    showTotal: (total, range) => { return (<div> {range[0]}-{range[1]} trên {total} rows</div>) }
+                    // onChange: (page) => console.log(page),
                 }}
                 headerTitle="Table user"
                 toolBarRender={() => [
