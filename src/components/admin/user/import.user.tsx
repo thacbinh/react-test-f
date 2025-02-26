1af657526cf9706e2d765059648416b835c8516e
binh <binhtdhe150589@fpt.edu.vn>
2025-02-26 14:53:16 +0700

    read-file-excel

--- src/components/admin/user/import.user.tsx
+++ src/components/admin/user/import.user.tsx
@@ -1,7 +1,10 @@
-import { Modal, Table } from "antd";
+import { App, Modal, Table } from "antd";
 import { InboxOutlined } from '@ant-design/icons';
 import { message, Upload } from 'antd';
 import type { UploadProps } from 'antd';
+import Exceljs from 'exceljs';
+import { useState } from "react";
+import { Buffer } from 'buffer';
 
 const { Dragger } = Upload;
 
@@ -10,8 +13,17 @@ interface IProps {
     setOpenModalImport: (v: boolean) => void;
 }
 
+interface IDataImport {
+    fullName: string;
+    email: string;
+    phone: string;
+}
+
 const ImportUser = (props: IProps) => {
     const { setOpenModalImport, openModalImport } = props;
+
+    const { message } = App.useApp();
+    const [dataImport, setDataImport] = useState<IDataImport[]>([]);
 
     const propsUpload: UploadProps = {
         name: 'file',
@@ -28,13 +40,48 @@ const ImportUser = (props: IProps) => {
             }, 1000);
         },
 
-        onChange(info) {
+        async onChange(info) {
             const { status } = info.file;
             if (status !== 'uploading') {
                 console.log(info.file, info.fileList);
             }
             if (status === 'done') {
+                console.log(info);
                 message.success(`${info.file.name} file uploaded successfully.`);
+                if (info.file) {
+                    const file = info.file.originFileObj!;
+
+                    //load file to buffer
+                    const workbook = new Exceljs.Workbook();
+                    const arrayBuffer = await file.arrayBuffer()
+                    const buffer = Buffer.from(arrayBuffer);
+                    await workbook.xlsx.load(buffer);
+
+                    //convert file to json
+                    let jsonData: IDataImport[] = [];
+                    workbook.worksheets.forEach(function (sheet) {
+                        // read first row as data keys
+                        let firstRow = sheet.getRow(1);
+                        if (!firstRow.cellCount) return;
+
+                        let keys = firstRow.values as any[];
+
+                        sheet.eachRow((row, rowNumber) => {
+                            if (rowNumber == 1) return;
+                            let values = row.values as any;
+                            let obj: any = {};
+                            for (let i = 1; i < keys.length; i++) {
+                                obj[keys[i]] = values[i];
+                            }
+                            jsonData.push(obj);
+                        })
+
+                    });
+
+                    setDataImport(jsonData)
+
+                }
+
             } else if (status === 'error') {
                 message.error(`${info.file.name} file upload failed.`);
             }
@@ -50,13 +97,17 @@ const ImportUser = (props: IProps) => {
                 width={"50vw"}
                 open={openModalImport}
                 onOk={() => setOpenModalImport(false)}
-                onCancel={() => setOpenModalImport(false)}
+                onCancel={() => {
+                    setDataImport([]);
+                    setOpenModalImport(false)
+                }}
                 okText="Import data"
                 okButtonProps={{
-                    disabled: true
+                    disabled: dataImport.length > 0 ? false : true
                 }}
                 //do not close when click outside
                 maskClosable={false}
+                destroyOnClose={true}
             >
                 <Dragger {...propsUpload} >
                     <p className="ant-upload-drag-icon">
@@ -70,6 +121,7 @@ const ImportUser = (props: IProps) => {
                 <div style={{ paddingTop: 20 }}>
                     <Table
                         title={() => <span>Dữ liệu upload:</span>}
+                        dataSource={dataImport}
                         columns={[
                             { dataIndex: 'fullName', title: 'Tên hiển thị' },
                             { dataIndex: 'email', title: 'Email' },
